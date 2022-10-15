91273287d9868e8a7a0268c035fc788f54446e3b
divyatellabati26 <95356480+divyatellabati26@users.noreply.github.com>
2022-10-15 10:26:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import DigitalClock from "./DigitalClock";
+export default function App() {
+  return (
+    <div className="App">
+      <DigitalClock />
+    </div>
+  );
+}
